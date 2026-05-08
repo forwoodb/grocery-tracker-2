0545ae5a30f2ed3a7f56642bb926ce75a9058c4e
Brenton Forwood <forwoodb77@gmail.com>
2026-05-08 17:22:05 -0400

    Updated links in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -51,23 +51,16 @@ const Navbar = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Item 1</a>
+              <Link href={`/`}>Home</Link>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              <Link href={"/dashboard/store"}>Store</Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>Shop</a>
+            </li>
+            <li>
+              <a>Kitchen</a>
             </li>
           </ul>
         </div>
