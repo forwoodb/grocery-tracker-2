62abb62e4ce93e5180366e41bd2ec6258472d083
Brenton Forwood <forwoodb77@gmail.com>
2026-05-10 04:06:41 -0400

    Added admin and nextCookies plugins to auth.ts which implemented role and banned to better-auth user model

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,7 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { nextCookies } from "better-auth/next-js";
+import { admin } from "better-auth/plugins";
 import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URL!);
@@ -11,4 +13,5 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [admin(), nextCookies()],
 });
