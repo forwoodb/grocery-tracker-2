a1b1077414b40144eabe129570409f978120e81e
Brenton Forwood <forwoodb77@gmail.com>
2026-05-11 20:58:38 -0400

    Created table for user info and imported users

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import User from "../models/User";
 
-const AdminPage = () => {
+const AdminPage = async () => {
+  // Get users
+  const users = await User.find({});
+  console.log(users);
+
   return (
     <div>
       <h1>Admin Page</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => {
+            return (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
