eb587e1140bfea0b79963314e412a2259e212dd8
Brenton Forwood <forwoodb77@gmail.com>
2026-05-12 01:21:33 -0400

    Implemented functionality to update edited user

--- app/admin/edit/[id]/page.tsx
+++ app/admin/edit/[id]/page.tsx
@@ -1,5 +1,6 @@
 import connectDb from "@/app/lib/db";
 import User from "@/app/models/User";
+import { redirect } from "next/navigation";
 
 interface PageProps {
   params: Promise<{ id: string }>;
@@ -11,7 +12,20 @@ const EditUserPage = async ({ params }: PageProps) => {
   const { id } = await params;
 
   const user = await User.findOne({ _id: id });
-  console.log(id);
+
+  const updateUser = async (formData: FormData) => {
+    "use server";
+
+    await connectDb();
+
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const role = formData.get("role");
+
+    await User.findByIdAndUpdate(id, { name, email, role });
+
+    redirect("/admin");
+  };
 
   return (
     <>
@@ -20,19 +34,19 @@ const EditUserPage = async ({ params }: PageProps) => {
           <div className="container p-4">
             <h1 className="text-lg text-center">Edit User</h1>
             <form
-              // action={updateUser}
+              action={updateUser}
               className="flex flex-col gap-4 w-xs p-4 mx-auto"
             >
               <input
                 type="text"
                 name="name"
-                value={user.name}
+                defaultValue={user.name}
                 className="input"
               />
               <input
                 type="text"
                 name="email"
-                value={user.email}
+                defaultValue={user.email}
                 className="input"
               />
               <select name="role" id="role" className="select">
