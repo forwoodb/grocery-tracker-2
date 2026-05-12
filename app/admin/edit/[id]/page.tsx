3f72a4cebab095403c0583d076bdf774dd8361a6
Brenton Forwood <forwoodb77@gmail.com>
2026-05-12 01:12:00 -0400

    Updated edit user page and form

--- app/admin/edit/[id]/page.tsx
+++ app/admin/edit/[id]/page.tsx
@@ -15,36 +15,39 @@ const EditUserPage = async ({ params }: PageProps) => {
 
   return (
     <>
-      <h1>Edit User Page</h1>
-      <form action="">
-        <label className="floating-label">
-          <span>Your Email</span>
-          <input
-            type="text"
-            placeholder="mail@site.com"
-            className="input input-md"
-          />
-        </label>
-        <input
-          type="text"
-          name="name"
-          defaultValue={user.name}
-          className="input"
-        />
-        <input
-          type="text"
-          name="email"
-          defaultValue={user.email}
-          className="input"
-        />
-        <input
-          type="text"
-          name="role"
-          defaultValue={user.role}
-          className="input"
-        />
-        <button>Update</button>
-      </form>
+      <main>
+        <section>
+          <div className="container p-4">
+            <h1 className="text-lg text-center">Edit User</h1>
+            <form
+              // action={updateUser}
+              className="flex flex-col gap-4 w-xs p-4 mx-auto"
+            >
+              <input
+                type="text"
+                name="name"
+                value={user.name}
+                className="input"
+              />
+              <input
+                type="text"
+                name="email"
+                value={user.email}
+                className="input"
+              />
+              <select name="role" id="role" className="select">
+                <option value="user" selected={user.role === "user"}>
+                  User
+                </option>
+                <option value="admin" selected={user.role === "admin"}>
+                  Admin
+                </option>
+              </select>
+              <button className="btn">Update </button>
+            </form>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
