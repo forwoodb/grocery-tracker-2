b4c2d948f0f3062e6948602dab20e141cce01cf7
Brenton Forwood <forwoodb77@gmail.com>
2026-05-12 00:28:23 -0400

    Created edit user form, imported user info to populate the form

--- app/admin/edit/[id]/page.tsx
+++ app/admin/edit/[id]/page.tsx
@@ -1,9 +1,50 @@
-import React from "react";
+import connectDb from "@/app/lib/db";
+import User from "@/app/models/User";
 
-const EditUserPage = () => {
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+const EditUserPage = async ({ params }: PageProps) => {
+  await connectDb();
+
+  const { id } = await params;
+
+  const user = await User.findOne({ _id: id });
+  console.log(id);
+
   return (
     <>
       <h1>Edit User Page</h1>
+      <form action="">
+        <label className="floating-label">
+          <span>Your Email</span>
+          <input
+            type="text"
+            placeholder="mail@site.com"
+            className="input input-md"
+          />
+        </label>
+        <input
+          type="text"
+          name="name"
+          defaultValue={user.name}
+          className="input"
+        />
+        <input
+          type="text"
+          name="email"
+          defaultValue={user.email}
+          className="input"
+        />
+        <input
+          type="text"
+          name="role"
+          defaultValue={user.role}
+          className="input"
+        />
+        <button>Update</button>
+      </form>
     </>
   );
 };
