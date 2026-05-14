798adf1962628cb238112807ee3e0a3446cbc9f0
Brenton Forwood <forwoodb77@gmail.com>
2026-05-13 23:33:51 -0400

    Implemented functionality to get grocery items from db and created table to display the items

--- app/dashboard/store/page.tsx
+++ app/dashboard/store/page.tsx
@@ -15,6 +15,8 @@ const StorePage = async () => {
     redirect("/auth/login");
   }
 
+  const groceryItems = await GroceryItem.find({});
+
   const createGroceryItem = async (formData: FormData) => {
     "use server";
     await connectDb();
@@ -160,6 +162,62 @@ const StorePage = async () => {
             </button>
           </div>
         </form>
+      </div>
+      <div className="bg-base-100 rounded-xl shadow-md p-6">
+        {/* <button click={addToList} className="btn btn-warning"> */}
+        <button className="btn btn-warning">Add Checked Items to List</button>
+        <table className="table table-sm table-pin-rows">
+          {/* <table> */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Item</th>
+              <th>Price</th>
+              <th>Price Type</th>
+              <th>Brand</th>
+              <th>Size</th>
+              <th>Location</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {groceryItems
+              .sort((a, b) => {
+                return a.itemName.localeCompare(b.itemName);
+              })
+              .map((item) => {
+                return (
+                  <tr key={item._id}>
+                    <td>
+                      <input
+                        type="checkbox"
+                        // onChange={() => handleCheckbox(item._id)}
+                      />
+                    </td>
+                    <td>{item.itemName}</td>
+                    <td>${item.price?.toFixed(2)}</td>
+                    <td>{item.priceType}</td>
+                    <td>{item.brand}</td>
+                    <td>{item.size}</td>
+                    <td>{item.location}</td>
+                    <td>
+                      {/* <Button click={() => editItemID(item)}>Edit</Button> */}
+                      <button>Edit</button>
+                    </td>
+                    <td>
+                      <button
+                        // click={() => deleteItem(item._id)}
+                        className="bg-red-700 text-white"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
       </div>
     </>
   );
