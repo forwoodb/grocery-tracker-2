bafe0f5abf42c09f2820bfc517edd8326ad3d1a3
Brenton Forwood <forwoodb77@gmail.com>
2026-05-14 03:01:48 -0400

    Implemented functionality to delete a grocery item via server action

--- app/dashboard/store/page.tsx
+++ app/dashboard/store/page.tsx
@@ -1,9 +1,21 @@
+import ItemForm from "@/app/components/ItemForm";
 import { auth } from "@/app/lib/auth";
 import connectDb from "@/app/lib/db";
 import GroceryItem from "@/app/models/GroceryItem";
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+
+interface Item {
+  _id: string;
+  itemName: string;
+  price: number;
+  priceType: string;
+  brand: string;
+  size: number;
+  units: string;
+  location: string;
+}
 
 const StorePage = async () => {
   await connectDb();
@@ -16,155 +28,37 @@ const StorePage = async () => {
     redirect("/auth/login");
   }
 
-  const groceryItems = await GroceryItem.find({});
+  const groceryItems: Item[] = await GroceryItem.find({});
+  // const data = await GroceryItem.find({}).lean();
+  // const groceryItems: Item[] = JSON.parse(JSON.stringify(data));
 
   const createGroceryItem = async (formData: FormData) => {
     "use server";
     await connectDb();
 
     const userId = session?.user.id;
-
     const data = Object.fromEntries(formData);
-
     const newItem = await new GroceryItem({ ...data, userId });
 
     await newItem.save();
 
     revalidatePath("/");
   };
+
+  const deleteGroceryItem = async (formData: FormData) => {
+    "use server";
+    await connectDb();
+
+    const id = formData.get("id");
+
+    await GroceryItem.findByIdAndDelete(id);
+
+    revalidatePath("/dashboard/store");
+  };
+
   return (
     <>
-      <div className="bg-base-100 rounded-xl shadow-md p-6 mb-3">
-        <h2 className="text-3xl  font-semibold mb-6 text-center">
-          {/* {edit ? "Edit Item" : "Store Items"} */}
-          Store Items
-        </h2>
-        <form action={createGroceryItem} className="space-y-6">
-          {/* Row 1 */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            <div
-              className="
-                      flex 
-                      flex-col 
-                      gap-1
-                      "
-            >
-              <label htmlFor="name" className="font-medium">
-                Name
-              </label>
-              <input
-                required
-                type="text"
-                name="itemName"
-                id="itemName"
-                // defaultValue={item.itemName}
-                // onChange={change}
-                className="input input-warning w-full"
-              />
-              <p className="label">Required</p>
-            </div>
-            <div className="flex flex-col gap-1">
-              <label htmlFor="price" className="font-medium">
-                Price
-              </label>
-              <div className="flex">
-                <input
-                  type="text"
-                  name="price"
-                  id="price"
-                  // defaultValue={item.price}
-                  className="
-                          input 
-                          input-warning 
-                          w-full
-                        "
-                />
-                <select
-                  className="select select-warning w-28"
-                  name="priceType"
-                  id="priceType"
-                  // defaultValue={item.priceType}
-                >
-                  <option value="">type</option>
-                  <option value="regular">regular</option>
-                  <option value="sale">sale</option>
-                  <option value="coupon">coupon</option>
-                </select>
-              </div>
-            </div>
-            <div className="flex flex-col gap-1">
-              <label htmlFor="brand" className="font-medium">
-                Brand
-              </label>
-              <input
-                type="text"
-                name="brand"
-                id="brand"
-                // defaultValue={item.brand}
-                className="input input-warning w-full"
-              />
-            </div>
-          </div>
-          {/* Row 2 */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div
-              className="
-                      input-group 
-                      flex 
-                      flex-col 
-                      gap-1
-                    "
-            >
-              <label htmlFor="name" className="font-medium">
-                Size
-              </label>
-              <div className="flex">
-                <input
-                  type="text"
-                  name="size"
-                  id="size"
-                  // defaultValue={item.size}
-                  className="input input-warning w-full"
-                />
-                <select
-                  className="select select-warning w-28"
-                  name="units"
-                  id="units"
-                  // defaultValue={item.units}
-                >
-                  <option value="units">units</option>
-                  <option value="servings">servings</option>
-                  <option value="oz">oz</option>
-                  <option value="tbsp">tbsp</option>
-                  <option value="lbs">lbs</option>
-                  <option value="gal">gal</option>
-                  <option value="ml">ml</option>
-                  <option value="l">l</option>
-                </select>
-              </div>
-            </div>
-            <div className="flex flex-col gap-1">
-              <label htmlFor="price" className="font-medium">
-                Location
-              </label>
-              <input
-                type="text"
-                name="location"
-                id="location"
-                // defaultValue={item.location}
-                className="input input-warning w-full"
-              />
-            </div>
-          </div>
-          {/* Actions */}
-          <div className="flex justify-start pt-4">
-            <button className="btn btn-warning px-8">
-              {/* {edit ? "Update" : "Add"} Item */}
-              Add
-            </button>
-          </div>
-        </form>
-      </div>
+      <ItemForm submit={createGroceryItem} />
       <div className="bg-base-100 rounded-xl shadow-md p-6">
         {/* <button click={addToList} className="btn btn-warning"> */}
         <button className="btn btn-warning">Add Checked Items to List</button>
@@ -210,12 +104,19 @@ const StorePage = async () => {
                       </button>
                     </td>
                     <td className="w-px whitespace-nowrap">
-                      <button
-                        // click={() => deleteItem(item._id)}
-                        className="btn btn-sm bg-red-700 text-white"
-                      >
-                        Delete
-                      </button>
+                      <form action={deleteGroceryItem}>
+                        <input
+                          type="hidden"
+                          name="id"
+                          defaultValue={item._id.toString()}
+                        />
+                        <button
+                          // click={() => deleteItem(item._id)}
+                          className="btn btn-sm bg-red-700 text-white"
+                        >
+                          Delete
+                        </button>
+                      </form>
                     </td>
                   </tr>
                 );
