c116cb4e6c882b9a030b3ed6d326079964fb88a2
Brenton Forwood <forwoodb77@gmail.com>
2026-05-16 17:08:14 -0400

    Implemented server action to remove a grocery item from the shopping list

--- app/dashboard/shopping-list/page.tsx
+++ app/dashboard/shopping-list/page.tsx
@@ -1,13 +1,24 @@
 import connectDb from "@/app/lib/db";
 import GroceryItem from "@/app/models/GroceryItem";
 import { Item } from "@/app/lib/types";
+import { revalidatePath } from "next/cache";
 
 const ShoppingListPage = async () => {
   await connectDb();
 
   const data = await GroceryItem.find({ inList: true }).lean();
   const shoppingItems: Item[] = JSON.parse(JSON.stringify(data));
-  console.log(shoppingItems);
+
+  const removeFromList = async (formData: FormData) => {
+    "use server";
+
+    const id = formData.get("id");
+
+    await GroceryItem.findByIdAndUpdate(id, { inList: false });
+    console.log(id);
+
+    revalidatePath("/dashboard/shopping-list");
+  };
 
   return (
     <div>
@@ -27,6 +38,12 @@ const ShoppingListPage = async () => {
                   <input type="checkbox" name="" id="" />
                 </td>
                 <td>{item.itemName}</td>
+                <td>
+                  <form action={removeFromList}>
+                    <input type="hidden" name="id" defaultValue={item._id} />
+                    <button className="btn">Remove</button>
+                  </form>
+                </td>
               </tr>
             );
           })}
