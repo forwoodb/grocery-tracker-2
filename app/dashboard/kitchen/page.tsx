1c2a1e39ec4f9061a01f650bb87b8e28d820deb7
Brenton Forwood <forwoodb77@gmail.com>
2026-05-17 00:05:15 -0400

    Created a table for the Kitchen page

--- app/dashboard/kitchen/page.tsx
+++ app/dashboard/kitchen/page.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-
-const KitchenPage = () => {
+const KitchenPage = async () => {
   return (
     <div>
       <h1>Kitchen Page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
     </div>
   );
 };
