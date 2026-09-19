4d6b6d8e0c6dde6ab7c1dad593f524691efa2eba
Brenton Forwood <forwoodb77@gmail.com>
2026-09-18 20:27:31 -0400

    Created ShoppingTableM component for mobile views

---
+++ app/components/ShoppingTableM.tsx
@@ -0,0 +1,85 @@
+"use client";
+import { Item } from "../lib/types";
+import ShopItem from "@/app/components/ShopItem";
+import { useState } from "react";
+
+interface ShoppingTableProps {
+  items: Item[];
+  remove: (formData: FormData) => Promise<void>;
+}
+
+const ShoppingTable = ({ items, remove }: ShoppingTableProps) => {
+  const [counts, setCounts] = useState<Record<string, number>>({});
+
+  const counter = (id: string, num: number) => {
+    setCounts((prev) => ({
+      ...prev,
+      [id]: (prev[id] || 1) + num,
+    }));
+  };
+
+  // // Get list total
+  const total = items.reduce((sum, item) => {
+    const count = counts[item._id] || 1;
+    const cost = item.price * count;
+    return sum + cost;
+  }, 0);
+
+  return (
+    <table className="table table-xs">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Count</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Price Type</th>
+          <th>Brand</th>
+          <th>Size</th>
+          <th>Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item) => {
+          return (
+            <tr key={item._id}>
+              <td>
+                <input
+                  type="checkbox"
+                  value={item._id}
+                  name="selected"
+                  form="kitchen"
+                />
+              </td>
+              <td>
+                <Counter count={count} add={add} subtract={subtract} />
+              </td>
+              <td>{item.itemName}</td>
+              <td>${(item.price * count).toFixed(2)}</td>
+              <td>{item.priceType}</td>
+              <td>{item.brand}</td>
+              <td>{`${item.size * count} ${item.units}`}</td>
+              <td>{item.location}</td>
+              <td>
+                <form action={remove}>
+                  <input type="hidden" name="id" defaultValue={item._id} />
+                  <button className="btn">Remove</button>
+                </form>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>Total</td>
+          <td>${total.toFixed(2)}</td>
+        </tr>
+      </tfoot>
+    </table>
+  );
+};
+
+export default ShoppingTable;
